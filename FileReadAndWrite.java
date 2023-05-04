87df67164d03f38f64057283d5e3621de0ee56fb
OrionJBanton <131889487+OrionJBanton@users.noreply.github.com>
2023-05-04 10:15:46 -0400

    working filereadandwrite

---
+++ FileReadAndWrite.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadAndWrite {
+    private String fileName;
+
+    public FileReadAndWrite(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void findFiles(String dirName, String extension) {
+        File[] files = new File(dirName).listFiles((dir1, filename) -> filename.endsWith(extension));
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
+    public int countLines() throws IOException {
+        int lineCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while (br.readLine() != null) {
+                lineCount++;
+            }
+        }
+        return lineCount;
+    }
+
+    public String readFile() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append(System.lineSeparator());
+                line = br.readLine();
+            }
+        }
+        return sb.toString();
+    }
+
+}
