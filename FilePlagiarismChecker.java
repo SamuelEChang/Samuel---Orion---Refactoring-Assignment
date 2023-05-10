8ab49d9e8ba2a17d75c2f24eba41fd30a3072209
SamuelEChang <131888393+SamuelEChang@users.noreply.github.com>
2023-05-09 20:20:15 -0400

    Update and rename FIlePlagiarismChecker.java to FilePlagiarismChecker.java

--- FilePlagiarismChecker.java
+++ FilePlagiarismChecker.java
@@ -17,33 +17,6 @@ public class FilePlagiarismChecker {
         this.libraryFiles = fileReadAndWrite.findFiles(libraryPath, "");
     }
 
-    public void checkPlagiarism() throws IOException {
-        // declare a local variable 'lineCount' and set it to the result of invoking the 'countLines' method with 'filePath' passed as argument
-        int lineCount = countLines(filePath);
-        System.out.println("The file " + filePath + " has " + lineCount + " lines.");
-
-        // declare a local variable 'fileContent' and set it to the result of invoking the 'readFile' method with 'filePath' passed as argument
-        String fileContent = readFile(filePath);
-
-        // iterate over each file name in the 'libraryFiles' ArrayList
-        for (String fileName : libraryFiles) {
-            // declare a local variable 'libraryFilePath' and set it to the value of the current file name in the loop
-            String libraryFilePath = fileName;
-            // declare a local variable 'libraryFileContent' and set it to the result of invoking the 'readFile' method with 'libraryFilePath' passed as argument
-            String libraryFileContent = readFile(libraryFilePath);
-            // declare a local variable 'plagiarizedLines' and set it to the result of invoking the 'getPlagiarizedLines' method with 'fileContent' and 'libraryFileContent' passed as arguments
-            ArrayList<String> plagiarizedLines = getPlagiarizedLines(fileContent, libraryFileContent);
-            if (plagiarizedLines.size() > 0) {
-                // print a message indicating the file and the plagiarized lines
-                System.out.println("The following lines in " + filePath + " are plagiarized from " + libraryFilePath + ":");
-                // iterate over each plagiarized line number in the 'plagiarizedLines' ArrayList
-                for (String lineNumber : plagiarizedLines) {
-                    System.out.println("Line " + lineNumber + ": " + fileContent.split("\\r?\\n")[Integer.parseInt(lineNumber) - 1]);
-                }
-            }
-        }
-    }
-
     protected int countLines(String filePath) throws IOException {
         FileReadAndWrite fileReadAndWrite = new FileReadAndWrite(filePath);
         return fileReadAndWrite.countLines();
